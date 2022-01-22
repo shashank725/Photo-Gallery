830e965ea0ac6773004e4dc41f8b3b920d9256be
shashank725 <shashank7rocks@gmail.com>
2022-01-23 00:55:54 +0530

    added forms.py

---
+++ main/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from pyexpat import model
+from django import forms
+from .models import gallery
+
+class galleryForm(forms.ModelForm):
+    class Meta:
+        model = gallery
+        field = '__all__'
